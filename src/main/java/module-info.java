8139dd5e7b2da2fe7ddc5cc72b116854f94d0391
Matheus Reato <matheusreato13@gmail.com>
2024-11-08 17:07:42 -0300

    fist commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.curso.workshopjavafxjdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.curso.workshopjavafxjdbc to javafx.fxml;
+    exports com.curso.workshopjavafxjdbc;
+}
